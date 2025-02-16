5de3d95d083f8fd7c091d0d489cc3ed012472398
Esteb <140031716+Timewolff@users.noreply.github.com>
2025-02-16 15:02:50 -0600

    Creacion de POST
    Se crea estructura del usuario, lista temporal de usuarios y el POST requerido.

--- TrabajoP2.py
+++ TrabajoP2.py
@@ -1,3 +1,20 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, HTTPException
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 import uuid
+
+app = FastAPI()
+
+class User(BaseModel):
+    id: str
+    name: str
+    email: str
+
+# Almacenar usuarios temporalmente
+users = []
+
+# Crear un usuario (POST)
+@app.post("/users/", status_code=status.HTTP_201_CREATED)
+def create_user(user: User):
+    users.append(user)
+    return user
